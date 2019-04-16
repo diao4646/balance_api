f9a9b924635915c1d44de83b337014a64e7e0af1
diao4646 <38576890+diao4646@users.noreply.github.com>
2019-04-17 02:19:31 +1000

    Add files via upload

---
+++ Controllers/balanceController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class balanceController : ApiController
+    {
+        public object[] Get(string input)
+        {
+            if (input.Length > 50)
+            {
+                return new ValidationError[]
+                {
+                    new ValidationError
+                    {
+                        name="ValidationError",
+                        msg="Must be between 1 and 50 chars long"
+
+                    }
+                };
+            }
+            List<char> b_list=new List<char>();
+            Boolean tmp = true;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i]=='('||input[i]=='{' || input[i] == '[')
+                {
+                    b_list.Add(input[i]);
+
+                }
+                if (input[i] == ')')
+                {
+                    if (b_list.Count == 0)
+                    {
+                        tmp = false;
+                        break;
+                    }
+                    if (b_list[b_list.Count - 1] == '(')
+                    {
+                        
+                        b_list.RemoveAt(b_list.Count - 1);
+                    }
+                    else
+                    {
+                        tmp = false;
+                        break;
+                    }
+                }
+                if (input[i] == ']')
+                {
+                    if (b_list.Count == 0)
+                    {
+                        tmp = false;
+                        break;
+                    }
+                    if (b_list[b_list.Count - 1] == '[')
+                    {
+                        b_list.RemoveAt(b_list.Count - 1);
+                    }
+                    else
+                    {
+                        tmp = false;
+                        break;
+                    }
+                }
+                if (input[i] == '}')
+                {
+                    if (b_list.Count == 0)
+                    {
+                        tmp = false;
+                        break;
+                    }
+                    if (b_list[b_list.Count-1] == '{')
+                    {
+                        b_list.RemoveAt(b_list.Count - 1);
+                    }
+                    else
+                    {
+                        tmp = false;
+                        break;
+                    }
+                }
+            }
+            if (b_list.Count != 0)
+            {
+                tmp = false;
+            }
+
+            return new ValidationResult[]
+            {
+                new ValidationResult
+                {
+   
+                    input=input,
+                    isBalanced=tmp
+                }
+            };
+        }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ValidationResult
+    {
+
+        
+        public string input { get; set; }
+
+        public Boolean isBalanced { get; set; }
+    }
+
+    public class ValidationError
+    {
+        public string name { get; set; }
+
+        public string msg { get; set; }
+    }
+}
